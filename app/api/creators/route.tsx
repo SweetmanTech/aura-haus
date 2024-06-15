c3acc4ea2704b82318ad9e2639e61b3a94f8ae2a
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-15 16:21:08 -0500

    I fix build errors.

--- app/api/creators/route.tsx
+++ app/api/creators/route.tsx
@@ -1,11 +1,11 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import getCreators from './getCreators';
 
 export const runtime = 'edge';
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
-    const mapped = getCreators();
+    const mapped = getCreators() as any;
     return NextResponse.json({ creators: mapped[0][0].value.value }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
