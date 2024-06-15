a26bf402ac2b27b86c48381033c8ce96c9b7521c
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-15 16:31:58 -0500

    I remove edge label to try fixing build errors.

--- app/api/creators/route.tsx
+++ app/api/creators/route.tsx
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server';
 import getCreators from './getCreators';
 
-export const runtime = 'edge';
-
 export async function GET() {
   try {
     const mapped = await getCreators();
