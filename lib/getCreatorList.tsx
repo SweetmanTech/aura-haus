274b9d074a8f881aea72d53d1ea77d742310e15a
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-15 17:10:30 -0500

    simple is best.

--- lib/getCreatorList.tsx
+++ lib/getCreatorList.tsx
@@ -1,13 +1,24 @@
-import { VERCEL_URL } from './consts';
+import { CURATOR } from './consts';
+import getAttestationLogs from './eas/getAttestationLogs';
+import getAttestations from './eas/getAttestations';
+import getDecodedAttestationData from './eas/getDecodedAttestationData';
 
 const getCreatorList = async () => {
   try {
-    const response = await fetch(`${VERCEL_URL}/api/creators`);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    return data.creators;
+    const logs = await getAttestationLogs(CURATOR);
+    const attestations = await getAttestations(logs);
+    const serializedAttestations = attestations.map((attestation: any) => ({
+      ...attestation,
+      result: attestation.result.map((value: any) =>
+        typeof value === 'bigint' ? value.toString() : value,
+      ),
+    }));
+    const mapped = serializedAttestations.map((attestation: any) =>
+      getDecodedAttestationData(attestation.result),
+    );
+    const returnValue = mapped[0][0].value.value;
+    console.log('SWEETS MAPPED', returnValue);
+    return returnValue;
   } catch (error) {
     console.error('Failed to fetch creator list:', error);
     return null;
