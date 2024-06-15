15ac8464c9e7e6095f48b8ec0d25da8cee965ee1
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-15 14:50:39 -0500

    I abstract hard-coded list of creators to standalone lib.

---
+++ lib/getCreatorList.tsx
@@ -0,0 +1,8 @@
+const getCreatorList = () => [
+  '0xa49f720bf331a5e701f452321d997a01336ed315',
+  '0x94734e11b78680a35b0a86ba3ce634d334d6d2a7',
+  '0x6ea4e2d5667e7d8cf5052383bfd09db548d2ec3e',
+  '0x089036a0835c6cf82e7fc42e9e95dfe05e110c81',
+];
+
+export default getCreatorList;
--- lib/zora/getSetupNewContractLogs.tsx
+++ lib/zora/getSetupNewContractLogs.tsx
@@ -1,6 +1,7 @@
 import { CHAIN_ID } from '../consts';
 import { getPublicClient } from '../viem';
 import { setupNewContractEventAbi } from '../abi/setup-new-contract-event';
+import getCreatorList from '../getCreatorList';
 
 const getSetupNewContractLogs = async () => {
   const publicClient = getPublicClient(CHAIN_ID);
@@ -14,11 +15,7 @@ const getSetupNewContractLogs = async () => {
     fromBlock,
     toBlock,
     args: {
-      creator: [
-        '0xa49f720bf331a5e701f452321d997a01336ed315',
-        '0x94734e11b78680a35b0a86ba3ce634d334d6d2a7',
-        '0x6ea4e2d5667e7d8cf5052383bfd09db548d2ec3e',
-      ],
+      creator: getCreatorList(),
     },
   });
 };
