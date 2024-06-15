3d58db1755d986ef7b516fee2e22469b1c20f9bd
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-15 16:26:15 -0500

    I await async function.

--- app/api/creators/route.tsx
+++ app/api/creators/route.tsx
@@ -5,7 +5,7 @@ export const runtime = 'edge';
 
 export async function GET() {
   try {
-    const mapped = getCreators() as any;
+    const mapped = await getCreators();
     return NextResponse.json({ creators: mapped[0][0].value.value }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
